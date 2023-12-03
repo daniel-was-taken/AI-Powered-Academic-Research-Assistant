56f8d36ac8119eb5e7b460472373d51b32d3fd9d
Prem Tatkari <tatkariprem2002@gmail.com>
2023-12-03 21:24:56 +0530

    Add pdf scraping

---
+++ M_pdfscrape.py
@@ -0,0 +1,9 @@
+import fitz 
+doc = fitz.open('BEtest.pdf') 
+text = "" 
+for page in doc: 
+    text += page.get_text() 
+
+output_file = 'text/textscrape.txt'
+with open(output_file, 'w', encoding='utf-8') as file:
+    file.write(text)
