617ea9acfb531bdb67a6767426e1e268f1bf81f1
daniel-was-taken <97611290+daniel-was-taken@users.noreply.github.com>
2024-02-05 16:52:31 +0530

    Create webscrape.py

---
+++ webscrape/webscrape.py
@@ -0,0 +1,19 @@
+# Go to https://arxiv.org/multi?group=grp_cs&%2Fform=Form+Interface
+
+# In advanced search,
+#     set Search terms = {Input from user(preprocessed)}
+#     subject = computer science (CS)
+#     EXTRA:  Date.
+
+    # or
+
+# use a preset query eg -announced_date_first; size: 50; include_cross_list: True; terms: AND title=Research on Quantum computing
+# idk how the url will be 
+
+
+# Download the first n(3) papers pdfs
+
+#save PDFS in a temp folder (papers/paper1.. paper3)
+
+
+
