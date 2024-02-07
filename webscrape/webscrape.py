1e409e9716a74b520da28613bc1f51ea788b6504
divyesh0406 <divyeshrmistry@gmail.com>
2024-02-07 21:29:42 +0530

    Add WebScrape

--- webscrape/webscrape.py
+++ webscrape/webscrape.py
@@ -9,11 +9,22 @@
 
 # use a preset query eg -announced_date_first; size: 50; include_cross_list: True; terms: AND title=Research on Quantum computing
 # idk how the url will be 
-
-
 # Download the first n(3) papers pdfs
-
 #save PDFS in a temp folder (papers/paper1.. paper3)
 
+import requests
+import re
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+keyword = input("Enter what you want to search for: ")
+keyword = keyword.strip()
+nkey = re.sub("\s", "+", keyword)
+print(nkey)
+test = "mobiles"
+# url = f"https://www.flipkart.com/{test}/mi~brand/pr?sid=tyy,4io&otracker=nmenu_sub_Electronics_0_Mi"
+url = f"https://arxiv.org/search/?query={nkey}&searchtype=all&source=header"
+r = requests.get(url)
+print(r)
 
 
