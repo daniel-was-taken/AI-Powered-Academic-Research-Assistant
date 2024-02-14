633efa4edd7dfd66d7c9e44591ee6171c5747304
daniel-was-taken <97611290+daniel-was-taken@users.noreply.github.com>
2024-02-14 20:12:44 +0530

    Change in app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-import routes
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
     return render_template("index.html")
+
+@app.route("/summary", methods=['GET','POST'])
+def summary():
+    if request.method == "POST":
+       input = request.form["input"]
+    if not input:
+        input="HELLO WORLD"
+    return render_template("summary.html", summary = input)
+
 
 if __name__ == '__main__':  
    app.run(debug=True)
