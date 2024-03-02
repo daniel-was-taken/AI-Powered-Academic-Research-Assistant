2e4a7893c5e05bb17d88f99491dbc6a2ee3909e8
divyesh0406 <divyeshrmistry@gmail.com>
2024-03-02 18:54:20 +0530

    add Data Cleaning

--- webscrape/preprocess.py
+++ webscrape/preprocess.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import re
+import numpy as np  
+import nltk
+from spacy.lang.en.stop_words import STOP_WORDS
 
 def clean_data(filename, output_filename):
-
     try:
         with open(filename, 'r', encoding='latin-1') as f:
             data = f.read()
-        # with open(filename, 'r') as f:
-        #     data = f.read()
-
-        cleaned_data = re.sub(r'\s{2,}', '\n', data) #Extra spaces
+        newText = data.lower()
+        newText = re.sub('[^\w\s\d\.]','',newText)
+        newText = ' '.join(newText.split())
+        tokens = [w for w in newText.split() if not w in STOP_WORDS]
+        long_words=[]
+        for i in tokens:
+            if len(i)>=3:
+                long_words.append(i)   
+                
+        cleaned_data = ' '.join([str(elem) for elem in long_words]).strip()
+        # cleaned_data = re.sub(r'\s{2,}', '\n', data) #Extra spaces
         cleaned_data = re.sub(r'[^a-zA-Z0-9\s]','', cleaned_data)  #Anything except alpha numeric except whitespace
 
         with open(output_filename, 'w') as f:
@@ -22,4 +31,20 @@ def clean_data(filename, output_filename):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-clean_data('text/textscrape2.txt', 'cleaned.txt')
+clean_data('text/textcontent.txt', 'cleaned2.txt')
+
+
+# def clean_body(text):
+#     newText = text.lower()
+#     newText = re.sub('[^\w\s\d\.]','',newText)
+#     newText = ' '.join(newText.split())
+#     tokens = [w for w in newText.split() if not w in STOP_WORDS]
+#     long_words=[]
+#     for i in tokens:
+#         if len(i)>=3:
+#             long_words.append(i)   
+#     return (" ".join(long_words)).strip()
+
+# cleaned_body = []
+# for t in data['body']:
+#     cleaned_body.append(clean_body(t))
